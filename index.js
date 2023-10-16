fdb175b5ab4a3685c154afb904d567410e399b71
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-16 13:05:09 +1100

    completed lab

--- index.js
+++ index.js
@@ -12,5 +12,26 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  // let mod = []
+  return tutorials.map((elem) => {
+    let words = elem.split(" ")
+    let capitalizedWord = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    let result = capitalizedWord.join(" ")
+    debugger
+
+
+    // for (const word of words) {
+    //   let firstLetter = word.charAt(0).toUpperCase()
+    //   debugger
+    
+    // }
+    return result
+    
+  })
+  
+  
+
 }
+
+
+console.log(titleCased())
